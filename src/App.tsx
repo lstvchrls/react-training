0606c9109b43551d9184746496c30c2079186947
lstvchrls <lstvcharleschristian@gmail.com>
2025-12-15 10:55:55 +0800

    tailwind and shadcn init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <main className="container mx-auto p-4">
+            <h1 className="mx-auto">Hello World</h1>
+            <Button>Primary</Button>
+            <Button variant="secondary">Secondary</Button>
+            <Button variant="destructive">Destructive</Button>
+            <Button variant="outline">Outline</Button>
+            <Button variant="ghost">Ghost</Button>
+            <Button variant="link">Link</Button>
+        </main>
     );
 }
 
